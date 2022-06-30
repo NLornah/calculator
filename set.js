b3e3824203731174fec2b79bf442e3dbe849d148
Nlornah <nderitulornah040@gmail.com>
2022-06-30 19:40:26 +0300

    project

---
+++ set.js
@@ -0,0 +1,8 @@
+// let myFavoriteFlavors = new Set();
+// myFavoriteFlavors.add('chocolate chip');
+// myFavoriteFlavors.add('cookies and cream');
+// myFavoriteFlavors.add('vanilla');
+// myFavoriteFlavors.add('strawberry');
+
+// myFavoriteFlavors.delete('strawberry');
+// console.log(myFavoriteFlavors)
